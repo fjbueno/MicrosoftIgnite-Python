369ac4a43f051b0ab1ba6513e9381cf430016c4c
Francisco Bueno <francisco.j.bueno@oracle.com>
2020-09-10 19:12:02 -0500

    Hello Word: Microsoft Inside

---
+++ HelloWord.py
@@ -0,0 +1,10 @@
+print ('Hello World!')
+# Prints Hello World! to the console. 
+
+greeting = 'Welcome to Ignite!'
+print (greeting)
+# Prints Welcome to Ignite! to the console.
+
+greeting = 'Learning Python is fun!'
+print (greeting)
+#Prints Learning Python is fun! to the console.
